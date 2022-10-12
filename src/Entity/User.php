09fc981a5f5eb63bc1ec625aea6ee3dfeaba2aee
evgen1067 <bogomolov.evgen1067@mail.ru>
2022-10-12 16:22:37 +0300

    добавил баланс пользователя и фикстуры

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column]
+    private ?float $balance = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -96,5 +99,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getBalance(): ?float
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(float $balance): self
+    {
+        $this->balance = $balance;
+
+        return $this;
     }
 }
